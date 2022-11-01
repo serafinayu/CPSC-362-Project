55d45f253bb415f5a301d12418ce54a43031edd1
Quoc Le <54720561+quoctle00@users.noreply.github.com>
2022-11-01 16:01:48 -0700

    need to figure out $_POST in sql queries

--- connection.php
+++ connection.php
@@ -17,6 +17,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     else {
         echo $_POST['food'];
+
     }
     echo $_POST['food'];
     echo $_POST['name'];
@@ -24,5 +25,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
    // $price = $_POST['Price'];
 }
 
+$sql = mysql_query("SELECT Street_address from FOOD where Food_type like '" . mysql_real_escape_string($_POST['food']) . "';");
 
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Address: " . $row["Street_address"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
 ?>
