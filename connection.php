da1985b2535f5340a685be5e13afab31d4ad8f7d
serafu <serafinayu@gmail.com>
2022-11-03 16:47:52 -0700

    update

--- connection.php
+++ connection.php
@@ -10,22 +10,26 @@ if ($conn->connect_error) {
 }
 echo "Connection sucessful";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $food = $_POST['food'];
+    $price = $_POST['price'];
+
     if (empty($food)) {
         echo "Food is empty";
     } else {
         echo $_POST['food'];
     }
     echo $_POST['food'];
-    echo $_POST['name'];
+    
+    $sql = "SELECT Street_address from FOOD where Food_type='" . $food;
+	$sql .= "' and price='" . $price . "'";
 
-    // $price = $_POST['Price'];
-    $sql = "SELECT Street_address from FOOD where Food_type = '" . $food . "'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         while ($row = $result->fetch_assoc()) {
+            echo "Restaurant: " . $row["Restaurant"] . "<br>";
             echo "Address: " . $row["Street_address"] . "<br>";
         }
     } else {
