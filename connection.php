de76baffbc25eb0fe20965d425e2c7cfb9084da6
Quoc Le <54720561+quoctle00@users.noreply.github.com>
2022-11-03 15:07:24 -0700

    update connection.php

--- connection.php
+++ connection.php
@@ -23,18 +23,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     echo $_POST['name'];
 
    // $price = $_POST['Price'];
-}
+   $sql = "SELECT Street_address from FOOD where Food_type = '" . $food . "'";
+   $result = $conn->query($sql);
 
-$sql = mysql_query("SELECT Street_address from FOOD where Food_type like '" . mysql_real_escape_string($_POST['food']) . "';");
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
+   if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "Address: " . $row["Street_address"]. "<br>";
-    }
-  } else {
+        while($row = $result->fetch_assoc()) {
+        echo "Address: " . $row["Street_address"]. "<br>";
+        }
+    } 
+  else {
     echo "0 results";
   }
+
+}
+
+
+
+
 ?>
