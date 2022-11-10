5009f6f5586f131aced12c571d6fffff28dc213f
ssaucedo208 <46984431+ssaucedo208@users.noreply.github.com>
2022-11-10 00:55:34 -0800

    Update results-page.php

--- results-page.php
+++ results-page.php
@@ -43,7 +43,7 @@
   <!-- Bottom Border and Credits
             - add text including class, professor, and school 
     -->
-  <footer>
+    <footer>
     <div class="credits">
       <address>
         Written by Quoc Le, Sebastian Saucedo, and Serafina Yu
@@ -56,43 +56,3 @@
 </body>
 
 </html>
-
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "foodDB";
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection sucessful";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-  $food = $_POST['food'];
-  $price = $_POST['price'];
-
-  if (empty($food)) {
-    echo "Food is empty";
-  } else {
-    echo $_POST['food'];
-  }
-  echo $_POST['food'];
-
-  $sql = "SELECT Street_address from FOOD where Food_type='" . $food;
-  $sql .= "' and price='" . $price . "'";
-
-  $result = $conn->query($sql);
-
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-      echo "Address: " . $row["Street_address"] . "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
-}
-
-?>
