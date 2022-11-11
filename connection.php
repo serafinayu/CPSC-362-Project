bf09bb579824595285b7505fc4d72bac25578001
Quoc Le <54720561+quoctle00@users.noreply.github.com>
2022-11-10 16:19:02 -0800

    query multi conditions works

--- connection.php
+++ connection.php
@@ -7,32 +7,42 @@ $database = "foodDB";
 $conn = mysqli_connect($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
-echo "Connection sucessful";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+}
+    echo "Connection sucessful" . "<br>";
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $food = $_POST['food'];
     $price = $_POST['price'];
-
-    if (empty($food)) {
+    if(empty($food)){
         echo "Food is empty";
-    } else {
+    }
+    else {
         echo $_POST['food'];
+
     }
-    echo $_POST['food'];
-    
-    $sql = "SELECT Street_address from FOOD where Food_type='" . $food;
-	$sql .= "' and price='" . $price . "'";
 
-    $result = $conn->query($sql);
+   //$sql = "SELECT Street_address from FOOD where Food_type = '".$food."'    ";
+   $sql = "SELECT Street_address from FOOD 
+            where Cost = '$price' AND Food_type = '$food'   ";
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            echo "Restaurant: " . $row["Restaurant"] . "<br>";
-            echo "Address: " . $row["Street_address"] . "<br>";
+   
+   //$sql .= "' and Cost='" . $price . "'";
+
+   $result = $conn->query($sql);
+
+   if ($result->num_rows > 0) {
+    // output data of each row
+        while($row = $result->fetch_assoc()) {
+        echo "Address: " . $row["Street_address"]. "<br>";
         }
-    } else {
-        echo "0 results";
-    }
+    } 
+  else {
+    echo "0 results";
+  }
+
 }
+
+
+
+
+?>
